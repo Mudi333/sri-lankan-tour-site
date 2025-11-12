2a7aa8e9d5e261394844b23bd148a1ca5daca571
Muditha <muditha.jayasooriya@dci-student.org>
2025-11-12 11:35:23 +0100

    test git

---
+++ tour-client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h2>Find your vacation suits you the best!</h2>
+  <Button>Click Me</Button>
+  </div>
+    </>
+  )
+}
+
+export default App
