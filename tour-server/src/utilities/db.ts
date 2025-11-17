b365eb7f30fd9a57d12b537c79256b0306529335
Muditha <muditha.jayasooriya@dci-student.org>
2025-11-17 22:04:18 +0100

    add client register and signin

--- tour-server/src/utilities/db.ts
+++ tour-server/src/utilities/db.ts
@@ -27,6 +27,27 @@
 
 
 
+// import dotenv from "dotenv";
+// import "dotenv/config";
+// import { Sequelize } from "sequelize";
+
+// dotenv.config()
+
+// const sequelize = new Sequelize(
+//    process.env.DB_NAME as string,
+//   process.env.DB_USER as string,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// export default sequelize;
+
+
 import dotenv from "dotenv";
 import "dotenv/config";
 import { Sequelize } from "sequelize";
