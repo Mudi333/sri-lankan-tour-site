35715a38d320819cde00e7da5024cf83ccfec8e0
Muditha <muditha.jayasooriya@dci-student.org>
2025-12-01 16:19:27 +0100

    change cors

--- tour-server/src/server.ts
+++ tour-server/src/server.ts
@@ -58,7 +58,7 @@ sequelize.sync({alter:true})
 
 //-------------------------------
 
-app.use(cors({origin: "http://localhost:5173",credentials:true}));
+app.use(cors());
 app.use(express.json());
 
 //--------------------------------
