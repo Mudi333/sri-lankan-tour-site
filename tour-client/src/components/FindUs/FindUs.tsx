823973276e2c76ad6abda76e1d73b8bcc43d4b14
Muditha <muditha.jayasooriya@dci-student.org>
2025-12-05 09:06:17 +0100

    style find us

--- tour-client/src/components/FindUs/FindUs.tsx
+++ tour-client/src/components/FindUs/FindUs.tsx
@@ -5,7 +5,7 @@ import style from "./FindUs.module.css";
 
 export default function FindUs() {
   return (
-    <section className={style.section_dark} id="why-us">
+    <section className={style.section_dark} id="find-us">
       <div className={style.div_container}>
         <h2 className={style.section_title}>Find us!</h2>
 
